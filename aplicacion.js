dac832f9635c4f291a819be134b709a945c7c5ac
desarrolladorweb6 <desarrolladorweb6@croydon.com.co>
2021-01-23 12:07:45 -0500

    4. Demostración de TypeScript

---
+++ aplicacion.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("Hola Mundo");
+function saludar(nombre) {
+    console.table(nombre);
+}
+const wolverine = {
+    nombre: "Logan"
+};
+saludar(wolverine.nombre);
---
+++ aplicacion.ts
@@ -0,0 +1,11 @@
+console.log("Hola Mundo")
+
+function saludar(nombre:string){
+    console.table(nombre)
+}
+
+const wolverine={
+    nombre:"Logan"
+}
+
+saludar(wolverine.nombre)
